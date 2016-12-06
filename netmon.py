b9e0c85c44b7237baf168ff283660973070a106e
Adam Kniffen <akniffen@cisco.com>
2016-12-06 11:28:27 -0600

    initializing with base functionality

---
+++ netmon.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python
+#
+"""
+@author: Adam Kniffen
+@contact: akniffen@cisco.com
+@copyright: Copyright 2016
+@organization: Cisco Active Threat Analytics
+@status: Development
+"""
+
+import re
+import ast
+import json
+import time
+import socket
+import os
+import sys
+from elasticsearch import Elasticsearch
+from datetime import datetime as dt
+
+class netmon:
+
+    def __init__(self, logpath=False, logappend=False, elasticsearchhost=False, elasticsearchindex=False,
+                 elasticsearchuser=False,
+                 elasticsearchpwd=False):
+        self.logpath = logpath
+        self.logappend = logappend
+        self.elasticsearchhost = elasticsearchhost
+        self.elasticsearchindex=  elasticsearchindex
+        self.elasticsearchuser = elasticsearchuser
+        self.elasticsearchpwd = elasticsearchpwd
+
+    def collectall(self):
+        """
+        Executes time phased collection tasks and calculates an average units / second / interface statistic
+        :return: dict
+        """
+        resp = {}
+        # collect once to start and then sleep
+        start = time.time()
+        _start = self.collectonce()
+        time.sleep(1)
+        _end = self.collectonce()
+        end = time.time()
+        diff = end - start
+
+        for iface in _start:
+            resp[iface] = {}
+            for key in _start[iface]:
+                resp[iface][key] = int(_start[iface][key] + _end[iface][key] / diff)
+        resp['host'] = socket.gethostname()
+        if self.logpath is not False:
+            self.log2file(resp)
+
+        if self.elasticsearchhost is not False:
+            self.log2es(resp)
+        else:
+            return resp
+
+    def log2file(self, resp):
+        """
+        Log our output to file as JSON so it can be consume with your logforwarder of choice
+        :return: Nada, zip, zilch. If this fails, we're going to sys.exit(1)
+        """
+        try:
+            if not os.path.isdir(self.logpath):
+                os.mkdir(self.logpath)
+            fmode = "w"
+            if self.logappend is True:
+                fmode = "a"
+            with open(os.path.join(self.logpath, "netmon.log"), fmode) as f:
+                f.write(json.dumps(resp))
+                f.write("\n")
+        except Exception:
+            sys.exit(1)
+
+    def log2es(self, resp):
+        """
+        for a more direct logging chain, you can write the results directly to the elasticsearch node and index of
+        your choice.
+        :return:
+        """
+        try:
+            if self.elasticsearchuser is not False:
+                es = Elasticsearch([self.elasticsearchhost], http_auth=(self.elasticsearchuser, self.elasticsearchpwd))
+            else:
+                es = Elasticsearch(self.elasticsearchhost)
+            resp['timestamp'] = dt.utcnow()
+            es.index(index=self.elasticsearchindex, doc_type='netmon', body=resp)
+        except Elasticsearch.connection_error:
+            sys.exit(1)
+
+    def collectonce(self):
+        """
+        collects /proc/net/dev and reformats into a dict. All credit for this formatter goes to
+        http://stackoverflow.com/users/6946/anurag-uniyal
+        :return: dict of values.
+        """
+        tgt = "/Users/akniffen/data/procnetdevtest"
+        lines = open(tgt, "r").readlines()
+        # lines = open("/proc/net/dev", "r").readlines()
+
+        columnLine = lines[1]
+        _, receivecols, transmitcols = columnLine.split("|")
+        receivecols = map(lambda a: "recv_" + a.strip(), receivecols.split())
+        transmitcols = map(lambda a: "trans_" + a.strip(), transmitcols.split())
+
+        cols = receivecols + transmitcols
+
+        resp = {}
+        for line in lines[2:]:
+            if line.find(":") < 0:
+                continue
+            iface, data = line.split(":")
+            # remove whitespace around the iface value
+            iface = re.sub("\s+", "", iface)
+            faceData = dict(zip(cols, data.split()))
+            # clean this up, they're not all strings after all
+            for key in faceData:
+                faceData[key] = ast.literal_eval(faceData[key])
+            resp[iface] = faceData
+        return resp
+
+
+if __name__ == "__main__":
+    import pprint
+    n = netmon(logpath="/Users/Akniffen/data/netmon")
+    n.collectall()
